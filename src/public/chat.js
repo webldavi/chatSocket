d1f524be7b7199d1f7f3e97fa29fe50842a00cef
webldavi <luisdavi.contato@gmail.com>
2023-01-30 00:24:55 +0000

    update styles

--- src/public/chat.js
+++ src/public/chat.js
@@ -7,7 +7,7 @@ const room = urlSearch.get('room')
 
 document.getElementById('nameRoom').innerHTML = `Olá ${username} - ${room}`
 
-function logout(){
+function logout() {
     window.location.href = '/'
 }
 
@@ -18,33 +18,38 @@ socket.emit('selected_room', {
 
 const listMessages = []
 
-function sendMessage(){
+function sendMessage() {
     const message = document.getElementById('message').value
-    socket.emit('send_new_message', {username, room, message})
+    socket.emit('send_new_message', { username, room, message })
     document.getElementById('message').value = ''
 
 }
 
 
-socket.on('send_new_message', payload=>{
+socket.on('send_new_message', payload => {
+    const today = new Date();
+    const h = today.getHours() <= 9 ? '0' + today.getHours() : today.getHours();
+    const m = today.getMinutes() <= 9 ? '0' + today.getMinutes() : today.getMinutes();
+    const day = today.getDate() <= 9 ? '0' + today.getDate() : today.getDate();
+    const month = today.getMonth() <= 9 ? '0' + `${new Date().getMonth() +1}` : today.getMonth();
+    const year = today.getFullYear();
     listMessages.push(`
-        <div class="w-full border border-gray-500 rounded-lg bg-gray-800 text-gray-300 flex flex-col gap-1">
-            <div class="w-full p-1 flex items-center justify-start">
-                <span class="text-gray-200 font-bold text-sm">${payload.username}</span>
+        <div class="w-full px-1 rounded-lg text-gray-300 flex flex-col">
+            <div class="flex flex-row gap-1">
+                <div class="w-max">
+                    <span class="text-gray-400 font-bold text-xs">${payload.username}:</span>
+                </div>
+                <div class="w-max text-xs font-bold text-gray-100 flex items-center justify-start">
+                    <p>${payload.text}
+                    </p>
+                </div>
             </div>
-            <div class="w-full p-1 flex items-center justify-start">
+            <div class="w-full flex items-center justify-start">
 
-                <p>${payload.text}
-                </p>
-            </div>
-            <div class="w-full p-1 flex items-center justify-start">
-
-                <span class="text-gray-200 font-bold text-sm">
-                    ${payload.createdAt}</span>
+                <span class="text-gray-400 font-bold text-xs">
+                    ${h}:${m} - ${day}/${month}/${year}</span>
 
             </div>
-        </div>`)
-
-
-    document.getElementById('listMessages').innerHTML = listMessages.join()
+        </div>`);
+    document.getElementById('listMessages').innerHTML = listMessages.join();
 })
